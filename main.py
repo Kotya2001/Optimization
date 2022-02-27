61283f9e9506ddcafef00ce72dfecd2579537251
Vladimir Sudakov <vsudakov@bk.ru>
2022-02-27 20:10:27 +0300

    fix

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-from GenJSON.CreateJSON import GenerateJSON
 from GenJSON.DataTransform import get_data
 from GenJSON.SolverFunction import LinearProgrammingExample
+import json
 
-numberOfRegs = 5
-typesOfPlaces = 3
-T = 3
-upperBound, totalBudget, totalProjPerYear = 4, 160000000, 3
-path = 'GenJSON/data.json'
-
-obj = GenerateJSON(numberOfRegs, typesOfPlaces, T, upperBound, totalBudget, totalProjPerYear)
-obj.gap()
-
-transformFunction = get_data('GenJSON/data.json')
-solverFunction = LinearProgrammingExample(*transformFunction)
-print(solverFunction)
-
-
-
-
+solverFunction = LinearProgrammingExample(*get_data('GenJSON/data.json'))
+print(json.dumps(solverFunction, indent=4, ensure_ascii=False))
